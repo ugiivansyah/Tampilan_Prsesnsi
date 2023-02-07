68ae259a9e2670130c7a2e6cedfd550e28f73e73
UGI_ <ugiivansyah34@gmail.com>
2023-02-07 12:06:59 +0700

    apk

---
+++ app/src/main/java/com/example/tampilanpesensi/tampilan_activity1.kt
@@ -0,0 +1,14 @@
+package com.example.tampilanpesensi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+class tampilan_activity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tampilan1)
+    }
+}
